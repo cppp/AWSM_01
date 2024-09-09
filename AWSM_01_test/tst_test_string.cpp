6c0fd3e7a557279549ddf450abe4fecc092b3303
mohammadreza <mohammadrezapirayesh1392@gmail.com>
2024-09-09 14:42:53 +0330

    add test

---
+++ AWSM_01_test/tst_test_string.cpp
@@ -0,0 +1,25 @@
+#include <QtTest>
+
+// add necessary includes here
+
+class Test_string : public QObject
+{
+    Q_OBJECT
+
+public:
+    Test_string();
+    ~Test_string();
+
+private slots:
+    void test_case1();
+};
+
+Test_string::Test_string() {}
+
+Test_string::~Test_string() {}
+
+void Test_string::test_case1() {}
+
+QTEST_APPLESS_MAIN(Test_string)
+
+#include "tst_test_string.moc"
